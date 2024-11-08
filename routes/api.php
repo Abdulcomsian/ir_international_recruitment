e6af0376533c15b16c5b81767beb170c0e595d2d
fardeendbqp <fardeen.khan@dbqplabs.com>
2024-11-08 16:01:20 +0500

    QIA-05 : recommended activities api done

--- routes/api.php
+++ routes/api.php
@@ -28,21 +28,30 @@ Route::post('/verify-otp', [UserController::class, 'verifyOtp']);
 // Route::post('/send-email-forgot-password', [UserController::class, 'sendEmailPassword']);
 // Route::post('/verfiy-code', [UserController::class, 'verifyCode']);
 // Route::post('/update-password', [UserController::class, 'updatePassword']);
+ // Quebec
+ Route::prefix('quebec')->group(function (){
 
+    Route::get('/foods', [QuebecFoodController::class, 'index']);
+    Route::get('/climates', [QuebecClimateController::class, 'index']);
+    Route::get('/climates/seasonal/{id}', [QuebecClimateController::class, 'seasonal']);
+    Route::get('/climates/packing-list/{id}', [QuebecClimateController::class, 'packingList']);
+    Route::get('/climates/recommended-activities/{id}', [QuebecClimateController::class, 'recommendedActivities']);
+
+});
 
 ////////Service Routes////////////////
 Route::middleware(['auth:api'])->group(function(){
     Route::get('get-services',[ServiceController::class,'getService']);
 
-    // Quebec
-    Route::prefix('quebec')->group(function (){
+    // // Quebec
+    // Route::prefix('quebec')->group(function (){
 
-        Route::get('/foods', [QuebecFoodController::class, 'index']);
-        Route::get('/climates', [QuebecClimateController::class, 'index']);
-        Route::get('/climates/seasonal/{id}', [QuebecClimateController::class, 'seasonal']);
-        Route::get('/climates/packing-list/{id}', [QuebecClimateController::class, 'packingList']);
+    //     Route::get('/foods', [QuebecFoodController::class, 'index']);
+    //     Route::get('/climates', [QuebecClimateController::class, 'index']);
+    //     Route::get('/climates/seasonal/{id}', [QuebecClimateController::class, 'seasonal']);
+    //     Route::get('/climates/packing-list/{id}', [QuebecClimateController::class, 'packingList']);
 
-    });
+    // });
     //Quebec information culture///
     Route::get('quebec/history',[QuebecHistoryController::class,'quebecHistory']);
     Route::get('indeed/jobs',[IndeedJobController::class,'fetchJobs']);
